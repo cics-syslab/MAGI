b67937b710f51c6c0c24ef516bd6ded5ffa7bb5d
NightDawnEX <65367327+NightDawnEX@users.noreply.github.com>
2023-02-10 12:35:14 -0500

    Update PagePreview.py

--- gui/components/PagePreview.py
+++ gui/components/PagePreview.py
@@ -23,7 +23,7 @@ class PagePreview:
 
         output_dir_var.trace_add('write', update_dir)
         label_frame = ttk.LabelFrame(master=self.page, text="Output Directory", padding=5)
-        label_frame.grid(sticky='ew')
+        label_frame.pack(padx=10, pady=10)
 
         ttk.Entry(master=label_frame, textvariable=output_dir_var, width=20).grid(row=0, column=0, sticky="w", padx=0,
                                                                                   pady=5, rowspan=5)
@@ -34,8 +34,7 @@ class PagePreview:
                                                                                                             pady=5)
 
         self.generate_button = ttk.Button(master=self.page, text="Generate", command=self.generate)
-        self.generate_button.grid(pady=10)
-
+        self.generate_button.pack(pady=10,fill='x')
     def generate(self):
         from core.components.generate import generate_output
         generate_output(SettingManager.BaseSettings.output_dir)
