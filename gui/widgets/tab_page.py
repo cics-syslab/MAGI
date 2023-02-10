34c59ee9d431699f35d1160db1047c1820b88c86
NightDawnEX <65367327+NightDawnEX@users.noreply.github.com>
2023-02-10 11:09:19 -0500

    cleanup

--- gui/widgets/tab_page.py
+++ gui/widgets/tab_page.py
@@ -7,19 +7,13 @@ class TabPage:
     def __init__(self, tab_control, name=None, data_object=None):
         self.tab_control = tab_control
         self.page = ttk.Frame(self.tab_control)
-        self.page.pack(expand=0, fill=tk.BOTH)
+        self.page.pack(expand=1, fill=tk.BOTH)
         tab_control.add(self.page, text=name)
         
         self.data_object = data_object
         from ttkbootstrap.scrolled import ScrolledFrame
+
         self.column = ScrolledFrame(self.page)
-        # self.column.grid(row=0, column=0, sticky=tk.NSEW)
-        self.column.pack(expand=1, fill=tk.BOTH)
-        # self.column.bind(
-        #     '<Configure>', lambda e: self.column.configure(scrollregion=self.column.bbox("all"))
-        # )
-        # vsb = tk.Scrollbar(self.page, orient="vertical", command=self.column.yview)
-        # vsb.grid(row=0, column=1, sticky='ns')
-        # self.column.configure(yscrollcommand=vsb.set)
+        self.column.pack(expand=1, fill=tk.BOTH,anchor=tk.CENTER)
         if self.data_object:
             DataObjectAttribute(self.column, data_object)
--- modules/NetworkProjectEngine/__init__.py
+++ modules/NetworkProjectEngine/__init__.py
@@ -7,7 +7,3 @@ description = "Network Project Engine"
 from .config import Config as _Config
 from core.managers.setting_manager import SettingManager
 config = SettingManager.register(_Config)
-#from core.managers.setting_manager import SettingManager
-#from core.managers.setting_manager import register
-# def get_config():
-#     return Config
