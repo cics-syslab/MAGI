17453eef86dbcd824e24c3d2d9fe9e4c538ffb80
NightDawnEX <65367327+NightDawnEX@users.noreply.github.com>
2023-02-10 16:15:38 -0500

    Update base_settings.py

--- core/base_settings.py
+++ core/base_settings.py
@@ -4,9 +4,9 @@ from typing import List
 
 @dataclass
 class BaseSettings:
-    project_name: str = "foo"
+    project_name: str = ""
     project_desc: str = ""
-    output_dir: str = ""
+    output_dir: str = "output"
     enabled_module: str = field(default_factory=str, metadata={"excluded_from_ui": True})
     enabled_plugins: List[str] = field(default_factory=set, metadata={"excluded_from_ui": True})
     submission_files: List[str] = field(default_factory=list)
