adf4acb4681b61a3071052edeae374faa529cb0b
Calvin Chai <65367327+nightdawnex@users.noreply.github.com>
2023-02-13 15:00:36 -0500

    change dir

--- core/components/handle_submission.py
+++ core/components/handle_submission.py
@@ -3,11 +3,11 @@ import os.path as op
 import shutil
 
 from ..managers import SettingManager
-
+from core.info.directories import Directories
 
 def remove_existing_submission_files():
     for submission_file in SettingManager.BaseSettings.submission_files:
-        submission_file_path = op.join(SettingManager.Directories.project_files_dir, submission_file)
+        submission_file_path = op.join(Directories.project_files_dir, submission_file)
 
         if op.exists(submission_file_path):
             os.remove(submission_file_path)
@@ -15,8 +15,8 @@ def remove_existing_submission_files():
 
 def move_submission_files():
     for submission_file in SettingManager.BaseSettings.submission_files:
-        submission_file_path = op.join(SettingManager.Directories.project_files_dir, submission_file)
-        submitted_file_path = op.join(SettingManager.Directories.submission_dir, submission_file)
+        submission_file_path = op.join(Directories.project_files_dir, submission_file)
+        submitted_file_path = op.join(Directories.submission_dir, submission_file)
         shutil.copy2(submitted_file_path, submission_file_path)
 
 
@@ -24,8 +24,8 @@ def move_submission_files():
 def check_submitted_files():
     missed_file = []
     for submission_file in SettingManager.BaseSettings.submission_files:
-        submission_file_path = op.join(SettingManager.Directories.project_files_dir, submission_file)
-        submitted_file_path = op.join(SettingManager.Directories.submission_dir, submission_file)
+        submission_file_path = op.join(Directories.project_files_dir, submission_file)
+        submitted_file_path = op.join(Directories.submission_dir, submission_file)
         if not op.exists(submitted_file_path):
             missed_file.append(submission_file)
 
