a8ee27dd250e59b8adf45ddd0ff5d8a3c96fd497
Calvin Chai <65367327+nightdawnex@users.noreply.github.com>
2023-02-13 13:15:38 -0500

    update directory info

--- core/info/directories.py
+++ core/info/directories.py
@@ -5,12 +5,6 @@ from dataclasses import dataclass
 @dataclass
 class Directories:
     import pathlib
-
-    # /autograder
-    autograder_root = op.join(r"C:\Users\kcc\OneDrive\Dev\gsgen", "assets/mock_autograder")
-    if op.exists("/autograder"):
-        autograder_root = "/autograder"
-    
     # autograder_root + source
     src_path: str = pathlib.Path(__file__).parent.parent.parent.resolve()
     
@@ -20,7 +14,11 @@ class Directories:
 
     project_files_dir = op.join(src_path, "project_files")
 
+    # /autograder
+    autograder_root = op.join(src_path, "assets/mock_autograder")
+    if op.exists("/autograder"):
+        autograder_root = "/autograder"
+    
     submission_dir = op.join(autograder_root, "submission")
-
-
+    result_path = op.join(autograder_root, "results/results.json")
 instance = Directories()
