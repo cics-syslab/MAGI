91937d08415a691dee4d7b728afb9e9004fec1e2
Calvin Chai <65367327+nightdawnex@users.noreply.github.com>
2023-02-13 10:32:36 -0500

    adapt file paths

--- core/info/directories.py
+++ core/info/directories.py
@@ -4,11 +4,16 @@ from dataclasses import dataclass
 
 @dataclass
 class Directories:
+    import pathlib
+
     # /autograder
     autograder_root = op.join(r"C:\Users\kcc\OneDrive\Dev\gsgen", "assets/mock_autograder")
+    if op.exists("/autograder"):
+        autograder_root = "/autograder"
+    
     # autograder_root + source
-    src_path: str = r"C:\Users\kcc\OneDrive\Dev\gsgen"
-    # Not needed
+    src_path: str = pathlib.Path(__file__).parent.parent.parent.resolve()
+    
     template_dir = op.join(src_path, "assets/GRADESCOPE_TEMPLATE")
 
     core_dir = op.join(src_path, "core")
