7982530cfd86ef179d7b51cf7b91ec41d6677400
Calvin Chai <65367327+nightdawnex@users.noreply.github.com>
2023-02-13 14:01:37 -0500

    directories changes

--- core/components/generate.py
+++ core/components/generate.py
@@ -4,6 +4,7 @@ import os.path as op
 import shutil
 
 from ..managers import SettingManager
+from ..info import Directories
 import logging
 
 def create_output_dir(output_parent_dir: str) -> str:
@@ -34,18 +35,18 @@ def generate_output(output_parent_dir: str) -> None:
     
     output_dir = create_output_dir(output_parent_dir)
     output_dir = op.join(output_dir, "autograder")
-    shutil.copytree(SettingManager.Directories.template_dir, output_dir)
-    shutil.copytree(SettingManager.Directories.core_dir, op.join(output_dir, "core"))
-    shutil.copy(op.join(SettingManager.Directories.src_path, "main.py"), output_dir)
+    shutil.copytree(Directories.template_dir, output_dir)
+    shutil.copytree(Directories.core_dir, op.join(output_dir, "core"))
+    shutil.copy(op.join(Directories.src_path, "main.py"), output_dir)
     if SettingManager.BaseSettings.enabled_module:
         enabled_module = SettingManager.BaseSettings.enabled_module
         os.mkdir(op.join(output_dir, "modules"))
-        shutil.copytree(op.join(SettingManager.Directories.src_path, "modules", enabled_module),
+        shutil.copytree(op.join(Directories.src_path, "modules", enabled_module),
                         op.join(output_dir, "modules", enabled_module))
     if len(SettingManager.BaseSettings.enabled_plugins) > 0:
         os.mkdir(op.join(output_dir, "plugins"))
         for enabled_plugin in SettingManager.BaseSettings.enabled_plugins:
-            shutil.copytree(op.join(SettingManager.Directories.src_path, "plugins", enabled_plugin),
+            shutil.copytree(op.join(Directories.src_path, "plugins", enabled_plugin),
                             op.join(output_dir, "plugins", enabled_plugin))
     pack_autograder(output_dir)
 
