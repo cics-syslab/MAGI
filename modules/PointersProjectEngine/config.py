e0f827dea106f8596b590317f6d2c9fa4d8b74f5
Ethan Kemper <ethan_kemper@icloud.com>
2023-11-02 15:21:49 -0400

    Added test_cases.yml template and test template

--- modules/PointersProjectEngine/config.py
+++ modules/PointersProjectEngine/config.py
@@ -1,20 +1,18 @@
 from dataclasses import dataclass, field
 from core.managers import SettingManager
-# from .decorators import register_settings
 
 @SettingManager.register
 @dataclass
 class Config:
     # Source code default fields
-    # project_name: str = "My Project"
-    CONTAINER: str = "Container"
-    CONTAINER_TYPE: str = "Container Type"
-    ITEM_TYPE: str = "Item Type"
-    ITEM: str = "Item"
-    LINKED_LIST: str = "Linked List"
+    container_name: str = "Container" # The container name you are storing your items in
+    container_type: str = "Container Type" # The struct type your container is named
+    item_name: str = "Item" # The name of the item you are working with (i.e. file, animal, etc)
+    item_type: str = "Item Type" # The struct type your item is named
+    linked_list_name: str = "Linked List" # Linked list to store your items, each being a "node"
     # For the main.c template, these are the default values:
-    DESCRIPTOR1: str = "Descriptor1"
-    DESCRIPTOR2: str = "Descriptor2"
-    ITEM1: str = "Item1"
-    ITEM2: str = "Item2"
-    ITEM3: str = "Item3"
+    descriptor_1: str = "Descriptor1" # First choice of an adjective describing a given item
+    descriptor_2: str = "Descriptor2" # Second choice of an adjective describing a given item
+    item_1: str = "Item1" # The name of your first item
+    item_2: str = "Item2" # The name of your second item
+    item_3: str = "Item3" # The name of your third item
