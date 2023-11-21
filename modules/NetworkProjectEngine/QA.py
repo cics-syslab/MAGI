b6d0f16789936026d07c3da70864758f74808230
Calvin Chai <65367327+nightdawnex@users.noreply.github.com>
2023-11-21 13:59:05 -0500

    Update QA.py

--- modules/NetworkProjectEngine/QA.py
+++ modules/NetworkProjectEngine/QA.py
@@ -2,25 +2,24 @@ import random
 
 
 def generate_question() -> str:
-    rand1 = random.randint(1, 26)
-    ALPHABETS = [chr(val) for val in range(65, 91)]
-    encrypted_message = "".join(random.choices(ALPHABETS, k=50))
-
     rand1 = random.randint(1, 1000)
     rand2 = random.randint(1, 1000)
     operator = random.choice(('+', '-', '*', '/'))
 
-    question = str(rand1) + " " + encrypted_message
+    question = str(rand1)+" "+operator+" "+str(rand2)
     return question
 
 
 def solve_question(question: str) -> str:
+    interp = {
+            '+': lambda x, y: x + y,
+            '-': lambda x, y: x - y,
+            '*': lambda x, y: x * y,
+            '/': lambda x, y: int(x / y)
+        }
+    if not question:
+        print("no question provided")
+        return ""
     parsed = question.split()
-    rand1 = int(parsed[0])
-    encrypted_message = parsed[1]
-    alpha_offset = (rand1) % 26
-    # print("{} {} alpha offset: {}".format(self.rand1, self.rand2, alpha_offset))
-    decrypted_message = ""
-    for char in encrypted_message:
-        decrypted_message += chr(((ord(char) - 65 + alpha_offset) % 26) + 65)
-    return decrypted_message
+    ans=str(interp[parsed[1]](int(parsed[0]),int(parsed[2])))
+    return ans
