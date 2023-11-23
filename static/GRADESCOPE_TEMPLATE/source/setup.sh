8cc8a469b142da9b6858afb82d1e400688010fac
Calvin Chai <65367327+nightdawnex@users.noreply.github.com>
2023-11-23 00:20:53 -0500

    Update setup.sh

--- static/GRADESCOPE_TEMPLATE/source/setup.sh
+++ static/GRADESCOPE_TEMPLATE/source/setup.sh
@@ -3,23 +3,55 @@
 # autograder. It is run before the autograder starts, and before the student's
 # code is run.
 
+# update the system timezone
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+
 apt-get update
 
-# Gradescope comes with python 3.10, Let's trust it for now
+check_and_set_python() {
+    if command -v python3.10 &> /dev/null; then
+        echo "Python 3.10 is installed."
+        update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
+    elif command -v python3.8 &> /dev/null; then
+        echo "Python 3.8 is installed."
+        update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+    else
+        echo "Neither Python 3.8 nor Python 3.10 is installed. Make sure you are using the correct image (Ubuntu 20.04 or 22.04)."
+        exit 1
+    fi
+}
 
-# Install python
-# apt-get install -y python3.8 python3-pip python3-dev python3-setuptools zip
-# # Upgrade pip
-# python3.8 -m pip install -U --force-reinstall pip
+check_and_set_python
 
-# This is needed for c only
-apt-get install -y libgtest-dev cmake 
-cd /usr/src/gtest && cmake CMakeLists.txt && make && cp *.a /usr/lib || true
+
+install_and_setup_gtest() {
+    # Install libgtest-dev and cmake
+    sudo apt-get install -y libgtest-dev cmake || { echo "Error installing libgtest-dev and cmake"; exit 1; }
+
+    # Go to the gtest source directory and build gtest
+    cd /usr/src/gtest || { echo "Error navigating to /usr/src/gtest"; exit 1; }
+    cmake CMakeLists.txt && make || { echo "Error building gtest"; exit 1; }
+
+    # Copy .a files to /usr/lib if they exist
+    shopt -s nullglob
+    files=(*.a)
+    if [[ ${#files[@]} -gt 0 ]]; then
+        sudo cp *.a /usr/lib || { echo "Error copying .a files to /usr/lib"; exit 1; }
+    else
+        echo "No .a files to copy."
+    fi
+}
+
+
+install_and_setup_gtest
 
 # Install solution script dependencies
 if [[ -f "/autograder/source/requirements.txt" ]]; then
     pip install -r /autograder/source/requirements.txt
+else
+    echo "No requirements.txt file found."
+    exit 1
 fi
-cd /autograder/source
-python3 main.py --setup
+
+
+python3 /autograder/source/main.py --setup
