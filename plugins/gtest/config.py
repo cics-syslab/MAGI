7a2aafeca297f9fbdec71e979b7b9dc16d55473e
Calvin Chai <65367327+nightdawnex@users.noreply.github.com>
2024-01-04 16:05:02 -0500

    Update config.py

--- plugins/gtest/config.py
+++ plugins/gtest/config.py
@@ -5,4 +5,4 @@ from core.managers import SettingManager
 @dataclass
 class Config:
     # test_executable: str = "test"
-    test_list_file: str = "plugins/gtest/test_list.yml"
+    test_list_file: str = "workdir/test_list.yml"
