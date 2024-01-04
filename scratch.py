ca4dd4306a35e365b6fc591d726bd06c04221ee8
Calvin Chai <65367327+nightdawnex@users.noreply.github.com>
2024-01-04 16:04:59 -0500

    Create scratch.py

---
+++ scratch.py
@@ -0,0 +1,7 @@
+from core.components.render import render_templates
+from scratch.jinja.config import Config
+
+context = Config().__dict__
+render_templates('scratch/jinja/template', context, 'scratch/jinja/output')
+
+
