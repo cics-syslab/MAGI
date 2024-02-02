1dc7abf7a7c46c392c0e92e5c0c5b4698c456e7f
NightDawnEX <65367327+NightDawnEX@users.noreply.github.com>
2024-02-02 18:09:07 -0500

    cleanup

--- magi/__init__.py
+++
@@ -1,8 +0,0 @@
-# from . import managers
-# from . import common
-# from . import components
-# from . import info
-# from . import managers
-import pluggy
-
-hookimpl = pluggy.HookimplMarker("magi")
