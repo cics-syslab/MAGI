7bb0e7dbd5057c541d2e7d77a39c0410f5a50a32
calvinchai <dev@kchai.me>
2024-02-09 20:04:55 +0000

    move debug information

--- webui/Hello.py
+++ webui/Hello.py
@@ -1,14 +1,4 @@
-import os
-import sys
-
 import streamlit as st
-from streamlit import session_state
-
-# st.set_page_config(
-#     page_title="Hello",
-#     page_icon="👋",
-#     layout="wide",
-# )
 from functions.session import init_session
 
 init_session()
@@ -16,31 +6,3 @@ init_session()
 st.write("# Welcome to MAGI")
 
 st.write("This is the web interface for MAGI.")
-
-st.write("## Debug Information")
-st.markdown(
-    """
-    # Path
-    """
-)
-st.write(sys.path)
-
-st.markdown(
-    """
-    # Working Directory
-    """
-)
-
-st.write(os.getcwd())
-
-AddonManager = session_state.AddonManager
-
-st.write("## Addons")
-
-st.write("### Modules")
-for addon in AddonManager.available_modules:
-    st.write(addon.name)
-st.write("### Plugins")
-for plugin in AddonManager.available_plugins:
-    st.write(plugin.name)
-    
---
+++ webui/pages/9_About.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+import streamlit as st
+from streamlit import session_state
+from functions.session import init_session
+
+init_session()
+
+st.write("## Debug Information")
+st.markdown(
+    """
+    # Path
+    """
+)
+st.write(sys.path)
+
+st.markdown(
+    """
+    # Working Directory
+    """
+)
+
+st.write(os.getcwd())
+
+AddonManager = session_state.AddonManager
+
+st.write("## Addons")
+
+st.write("### Modules")
+for addon in AddonManager.available_modules:
+    st.write(addon.name)
+st.write("### Plugins")
+for plugin in AddonManager.available_plugins:
+    st.write(plugin.name)
+    
