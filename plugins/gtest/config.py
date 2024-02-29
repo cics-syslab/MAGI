a68128b2c2e03773a53430056573c6036068d7e2
Calvin Chai <dev@kchai.me>
2024-02-29 16:18:41 -0500

    Update test_list_file and test_executable paths in Config class

--- plugins/gtest/config.py
+++ plugins/gtest/config.py
@@ -7,9 +7,9 @@ from magi.managers import SettingManager
 @SettingManager.register
 @dataclass
 class Config:
-    test_list_file: str = field(default="test_list.yml", metadata={"help": "Path to the test list file, usually not needed to be changed"})
+    test_list_file: str = field(default="workdir/test_list.yml", metadata={"help": "Path to the test list file, usually not needed to be changed"})
     # TODO: Implement this
-    test_executable: Optional[str] = field(default=None, metadata={"help": "Path to the test executable, when unset, the plugin will read it from the test_list.yml file"})
+    test_executable: str = field(default="", metadata={"help": "Path to the test executable, when unset, the plugin will read it from the test_list.yml file"})
     test_list: str = field(default="plugins/gtest/test_list.yml",
                           metadata={"excluded_from_ui": True,
                                     "file_editor": "plugins/gtest/test_list.yml"})
