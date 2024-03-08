68cc7d85696b260505d66362f4c1fb48b7780db3
Calvin Chai <dev@kchai.me>
2024-03-08 11:34:08 -0500

    change comment into help

--- modules/PointersProjectEngine/config.py
+++ modules/PointersProjectEngine/config.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass,field
 
 from magi.managers import SettingManager
 
@@ -7,15 +7,19 @@ from magi.managers import SettingManager
 @dataclass
 class Config:
     # Source code default fields, these are changed depending on user's choice
-    container_name: str = "container"  # The container name you are storing your items in
-    container_type: str = "Container"  # The struct type your container is named
-    item_name: str = "item"  # The name of the item you are working with (i.e. file, animal, etc)
-    item_type: str = "Item"  # The struct type your item is named
-    node_type: str = "Node"  # node to store your items, each being a "node"
+    container_name: str = field(default="container",
+                                metadata={"help": "The container name you are storing your items in."})
+    container_type: str = field(default="Container", metadata={"help": "The struct type your container is named."})
+    item_name: str = field(default="item",
+                           metadata={"help": "The name of the item you are working with (i.e. file, animal, etc)."})
+    item_type: str = field(default="Item", metadata={"help": "The struct type your item is named."})
+    node_type: str = field(default="Node", metadata={"help": "Node to store your items, each being a 'node'."})
     # For the main.c templates, these are the default values:
-    container1: str = "Container1"  # First choice of an adjective describing a given item
-    container2: str = "Container2"  # Second choice of an adjective describing a given item
-    item1: str = "item1"  # The name of your first item
-    item2: str = "item2"  # The name of your second item
-    item3: str = "item3"  # The name of your third item
-    cluster_name: str = "ClusterName"
+    container1: str = field(default="Container1",
+                            metadata={"help": "First choice of an adjective describing a given item."})
+    container2: str = field(default="Container2",
+                            metadata={"help": "Second choice of an adjective describing a given item."})
+    item1: str = field(default="item1", metadata={"help": "The name of your first item."})
+    item2: str = field(default="item2", metadata={"help": "The name of your second item."})
+    item3: str = field(default="item3", metadata={"help": "The name of your third item."})
+    cluster_name: str = field(default="ClusterName", metadata={"help": "Name of the cluster."})
