736f098082650c345654ecef5a42272dea57afe4
andOrlando <bennettgillig@gmail.com>
2024-03-29 14:26:53 -0400

    Fixes wrong error message

--- plugins/js_install_compile/setup.sh
+++ plugins/js_install_compile/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apt-get update && apt-get install -y npm node || { echo "Error installing libgtest-dev and cmake"; exit 1; }
+apt-get update && apt-get install -y npm node || { echo "Error installing npm and node"; exit 1; }
